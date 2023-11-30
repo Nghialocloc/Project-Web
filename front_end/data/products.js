ac50a385cbbe1ebdf740c2d4e5991198028ce46a
Nguyễn Minh Đức <130299650+NMDuc207664@users.noreply.github.com>
2023-11-30 10:56:14 +0700

    Add files via upload

---
+++ front_end/data/products.js
@@ -0,0 +1,58 @@
+var list_products = [{
+    "name": "Nike Air Force 1 Wild",
+    "company": "Nike",
+    "img": "img/products/Nike-air-force-wild-1.webp",
+    "price": "4.490.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "Size": "42",
+        "Chất liệu": "Vải",
+        "Mẫu": "Nữ",
+        "Năm sản xuất": "2023",
+        "Màu:":"Đen",
+    },
+    "masp": "Nike"
+}, {
+    "name": "Nike Air Force 1'07 W",
+    "company": "Nike",
+    "img": "img/products/Nike-air-force-107.webp",
+    "price": "3.000.000",
+    "star": 3,
+    "rateCount": 21,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "Size": "41",
+        "Chất liệu": "Vải",
+        "Mẫu": "Nữ",
+        "Năm sản xuất": "2023",
+        "Màu:":"Trắng",
+    },
+    "masp": "Nike"
+}, {
+    "name": "Nike Air Force 1'07 LB",
+    "company": "Nike",
+    "img": "img/products/Nike-air-force-107-LB.webp",
+    "price": "3.490.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "Size": "42",
+        "Chất liệu": "Vải",
+        "Mẫu": "Nữ",
+        "Năm sản xuất": "2023",
+        "Màu:":"Nâu trắng",
+    },
+    "masp": "Nike"
+}]
