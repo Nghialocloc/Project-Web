7164eff6d302c6c6df6de4fdbecf9dcb1de39490
NGuyen Duc <130299650+NMDuc207664@users.noreply.github.com>
2024-01-12 01:48:47 +0700

    React 2
    fe 2

---
+++ react-frontend/src/Pages/Cart.jsx
@@ -0,0 +1,106 @@
+import "./style.css";
+import Navigationbar from "../Navigationbar/Navbar";
+import Productions from "../Productions/Products";
+import Footer from "../Footer/Footer";
+import Newsletter from "../Newsletter/Newsletter";
+
+function Cart() {
+  return (
+    <>
+      <Navigationbar />
+      <section id="cart" className="section-p1">
+        <table id="table" width="100%">
+          <thead>
+            <tr>
+              <td>Remove</td>
+              <td>Image</td>
+              <td>Sản phẩm</td>
+              <td>Giá cả</td>
+              <td>Số lượng</td>
+              <td>Tổng giá tiền</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <a href="#">
+                  <i className="far fa-times-circle"></i>
+                </a>
+              </td>
+              <td>
+                <img src="img/products/f1.jpg" alt="" />
+              </td>
+              <td>Giày nike</td>
+              <td>100$</td>
+              <td>
+                <input type="number" value="1" />
+              </td>
+              <td>$100$</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="#">
+                  <i className="far fa-times-circle"></i>
+                </a>
+              </td>
+              <td>
+                <img src="img/products/f1.jpg" alt="" />
+              </td>
+              <td>Giày nike</td>
+              <td>100$</td>
+              <td>
+                <input type="number" value="1" />
+              </td>
+              <td>$100$</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="#">
+                  <i className="far fa-times-circle"></i>
+                </a>
+              </td>
+              <td>
+                <img src="img/products/f1.jpg" alt="" />
+              </td>
+              <td>Giày nike</td>
+              <td>100$</td>
+              <td>
+                <input type="number" value="1" />
+              </td>
+              <td>$100$</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <section id="cart-add" className="section-p1">
+        <div id="coupon">
+          <h3>Aplly Coupon</h3>
+          <div>
+            <input type="text" placeholder="Nhập mã quà tặng" />
+            <button className="normal">Apply</button>
+          </div>
+        </div>
+        <div id="subtotal">
+          <h3>Hóa đơn</h3>
+          <table>
+            <tr>
+              <td>Số tiền</td>
+              <td>300$</td>
+            </tr>
+            <tr>
+              <td>Tiền ship</td>
+              <td>Miễn Phí</td>
+            </tr>
+          </table>
+          <button id="thanhtoan" className="normal">
+            Thanh toán
+          </button>
+        </div>
+      </section>
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
+export default Cart;
---
+++ react-frontend/src/Productions/Products.jsx
@@ -0,0 +1,203 @@
+import "./Products.css";
+function Products() {
+  return (
+    <>
+      <script
+        src="https://kit.fontawesome.com/1147679ae7.jsx"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+      />
+      <section id="product1" className="section-p1">
+        <h2>Featured Products</h2>
+        <p>Winter Collection New Model</p>
+        <div className="pro-container">
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+          <div className="pro">
+            <img src="img/products/f1.jpg" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut</h5>
+              <div className="star">
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fa fa-shopping-cart cart"></i>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Products;
